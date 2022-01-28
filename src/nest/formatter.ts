dd56ab6cbfa5eb499b4dbaee859fd60da09b678f
Horváth Dániel <daniel.horvath@wolterskluwer.com>
2022-01-28 11:21:39 +0100

    fix undefined error

--- src/nest/formatter.ts
+++ src/nest/formatter.ts
@@ -2,13 +2,15 @@ import { Formatter } from "@nitedani/logging-core";
 
 export class NestFormatter extends Formatter {
   transform(info, opts) {
-    if (typeof info === "object") {
+    if (typeof info === "object" && info.context) {
       // Dont log messages from the default ExceptionsHandler,
       // to avoid duplicate error messages
+
       if (JSON.stringify(info.context).includes("ExceptionsHandler")) {
         return false;
       }
     }
+
     return super.transform(info, opts);
   }
 }
